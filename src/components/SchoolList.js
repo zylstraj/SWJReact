6223911d7fc4abb13f5c1dc20499e93cb33bc0b9
zylstraj <colbertlove12@gmail.com>
2017-05-04 16:25:18 -0700

    Building it into a soley React app
    Re-working things on this branch to make it more functional without
    Redux.

--- src/components/SchoolList.js
+++ src/components/SchoolList.js
@@ -10,6 +10,52 @@ const Route = ReactRouter.Route;
 import SelectSchool from './selectSchool';
 
 class SchoolList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      school: [
+        {
+        title: "Michigan",
+        description: "University of Michigan has a unique history",
+        content: "This is the paragraph about my story and the article ad",
+        image: "./michigan.jpg",
+        enrollment: "44,718",
+        location: "Ann Arbor, MI",
+        id: "michigan"
+      },
+      {
+        title: "Ohio State",
+        description: "Shit State",
+        content: "Easily the worst school in the Big 10. Universally disliked.",
+        image: "./ohiostate.jpg",
+        enrollment: "66,046",
+        location: "Columbus, OH",
+        id: "ohiostate"
+      },
+      {
+        title: "Michigan State",
+        description: "Michigan State is bad",
+        content: "Grand River sucks",
+        image: "./michiganstate.gif",
+        enrollment: "50,344",
+        location: "East Lansing, MI",
+        id: "michiganstate"
+      }
+    ]
+  }
+}
+create2List() {
+  return this.state.school.map((school) => {
+    return(
+      <div>
+
+      <h1>{school.title}</h1>
+      <img src={school.image} alt={school.title} />
+      </div>
+    )
+  })
+}
+
   createList(){
     return this.props.schools.map((school) => {
       return(
@@ -25,9 +71,19 @@ class SchoolList extends React.Component {
   }
   render() {
     return (
+      <div>
+      <div>
+        <h1>{this.state.school[0].title}</h1>
+        <img src={this.state.school[0].image} alt={this.state.school[0].title} />
+      </div>
+      <div>
+      {this.create2List()}
+      </div>
       <ul>
+
         {this.createList()}
       </ul>
+      </div>
     )
   }
 }
