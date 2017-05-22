7b53b1ba96a8579af27a3577f2ef594c63239d4c
zylstraj <colbertlove12@gmail.com>
2017-05-22 15:54:19 -0700

    Loopin
    Working on looping all of the  values that exist.

--- src/components/Map.js
+++ src/components/Map.js
@@ -34,7 +34,9 @@ class GoogleMaps extends React.Component {
   // }
   componentDidMount() {
     console.log(google.maps);
-    console.log(this.props.content[0].lat)
+    console.log(this.props.content)
+    const differentPlaces = this.props.content;
+    console.log(differentPlaces)
 
     var map = new google.maps.Map(this.refs.map, {
       zoom: 15,
@@ -43,6 +45,35 @@ class GoogleMaps extends React.Component {
         lng: this.props.lng
       }
     });
+    // var marker = differentPlaces.map(function(place, index) {
+    //     new google.maps.Marker({
+    //       postion: {lat: place.lat, lng: place.lng},
+    //       map: map
+    //     })
+    //   })
+      // var infowindow = array.forEach(function(place, index) {
+      //   new google.maps.InfoWindow({
+      //     content: place.description
+      //   });
+      //   marker.addListener('click', function() {
+      //     infowindow.open(map, marker)
+      //   })
+      // })
+
+    // var marker = differentPlaces.forEach(function(place, index) {
+    //   new google.maps.Marker({
+    //     postion: {lat: place.lat, lng: place.lng},
+    //     map: map
+    //   })
+    // })
+    // var infowindow = differentPlaces.forEach(function(place, index) {
+    //   new google.maps.InfoWindow({
+    //     content: place.description
+    //   });
+    //   marker.addListener('click', function() {
+    //     infowindow.open(map, marker)
+    //   })
+    // })
     var marker = new google.maps.Marker({
       position: {lat: this.props.content[1].lat, lng: this.props.content[1].lng},
       map: map
@@ -53,6 +84,17 @@ class GoogleMaps extends React.Component {
        marker.addListener('click', function() {
          infowindow.open(map, marker)
        })
+// INFO WINDOW ONLY CAN BE ONCE AND NEEDS TO RELY ON THIS - FIGURE THIS OUT. LOTS OF PRACTICE CODE HAPPENING HERE
+    //    var markerTwo = new google.maps.Marker({
+    //      position: {lat: this.props.content[0].lat, lng: this.props.content[0].lng},
+    //      map: map
+    //  });
+    //    var infowindow = new google.maps.InfoWindow({
+    //         content: this.props.content[0].description
+    //       });
+    //       marker.addListener('click', function() {
+    //         infowindow.open(map, marker)
+    //       })
   }
   render() {
     return (
