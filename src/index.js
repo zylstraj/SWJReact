9fb9ae79fb0ba30e5434a3246613311ca029d572
zylstraj <colbertlove12@gmail.com>
2017-05-24 11:37:11 -0700

    Removed reducers - - and Provider.
    Code wasn't working as was.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import {createStore, applyMiddleware } from 'redux';
 import {BrowserRouter} from 'react-router-dom';
 import App from './components/app';
 
-
-import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </Provider>
   , document.getElementById('container'));
