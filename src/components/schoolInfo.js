fdd2d06b8ab946030a0e488dd61a31a818a5a047
Josh Zylstra <zylstrajosh@gmail.com>
2017-05-24 09:10:04 -0700

    Working in top nav
    Looks like shit. I'll touch it more later.

--- src/components/schoolInfo.js
+++ src/components/schoolInfo.js
@@ -97,6 +97,7 @@ render() {
     <div>
       <div className={styles.div_main_header}>
         <img src={this.state.image} alt={this.state.id} className={styles.schoolImage_header} />
+        <h1><Link to="/">Home</Link></h1>
         <img src={this.state.mainImage} alt="Michigan Stadium" className={styles.div_header_image} />
       </div>
       <div className={styles.div_main_content}>
